9af2aa3aa823ddcc9fac25a22e7dc8b01be13d19
mckwak <mckwak@mmlab.snu.ac.kr>
2018-11-20 17:09:17 +0900

    buf read/write func. added

--- fogos_socket/flexid_session/a/src/FlexIDSession/FlexIDSessionContext.java
+++ fogos_socket/flexid_session/a/src/FlexIDSession/FlexIDSessionContext.java
@@ -1,20 +1,58 @@
 package FlexIDSession;
 
-import java.io.Serializable;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 
 /**
  * FlexID socket 세션이 유지되는 동안의 context 정보 클래스
  * @author mckwak
  */
-public class FlexIDSessionContext implements Serializable {
-	// mckwak: getter/setter, Serializable interface 할당
-	private static final long serialVersionUID = 1L;
+public class FlexIDSessionContext {
+	public static final int BUF_SIZE = 1024;   
+
 	int length;
 	FlexID SFID; // source FlexID
 	FlexID DFID; // destination FlexID
 	int lastSeq;
 	int lastAck;
 	byte[] rbuf, wbuf; // make it with a circular queue
+
+	public FlexIDSessionContext() {
+		super();
+		this.rbuf = new byte[BUF_SIZE];
+		this.wbuf = new byte[BUF_SIZE];
+	}
+	
+	Object readBuf(int bytes) {
+		byte[] readBytes = rbuf;
+		ByteArrayInputStream bis = new ByteArrayInputStream(readBytes);
+		ObjectInput in = null;
+		try {
+		  in = new ObjectInputStream(bis);
+		  Object o = in.readObject();
+	      if (in != null) in.close();
+		  return o;
+		} catch (IOException ex) {}
+		catch (ClassNotFoundException e) {}
+		return null;
+ 	}
+	
+	void writeBuf(Object o) {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutput out = null;
+		try {
+		  out = new ObjectOutputStream(bos);   
+		  out.writeObject(o);
+		  out.flush();
+		  wbuf = bos.toByteArray();
+		  bos.close();
+		} catch (IOException ex) {}
+	}
 
 	public int getLength() {
 		return length;
